984d392c514f6a6f13e86c422814b792288b2932
Nazmul8888 <nazmulkhan416@gmail.com>
2023-11-12 18:24:51 +0600

    first commit

---
+++ src/Provider/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCl6HvTJTjcqozeTDS2UVeikIi5XIQpWyc",
+  authDomain: "stay-vista-7bf1f.firebaseapp.com",
+  projectId: "stay-vista-7bf1f",
+  storageBucket: "stay-vista-7bf1f.appspot.com",
+  messagingSenderId: "598678634100",
+  appId: "1:598678634100:web:7b199429ad3b5eef0ddb21"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
